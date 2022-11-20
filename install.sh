c55a1dfced79b7528507c343b8a1cdf4f1493ab4
ymktmk <ymktmk.tt@gmail.com>
2022-11-21 00:45:54 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
-DOT_DIR="$HOME/Git/dotfiles"
 
-git clone https://github.com/ymktmk/dotfiles.git ${DOT_DIR}
+git clone https://github.com/ymktmk/dotfiles.git $HOME/Git/dotfiles
+
+git clone https://github.com/wbthomason/packer.nvim \
+  ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 
 ln -sf $DOT_DIR/.zshenv ~/.zshenv
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
-ln -s $DOT_DIR/.config/fish ~/.config/fish
-ln -s $DOT_DIR/.config/nvim ~/.config/nvim
-ln -s $DOT_DIR/.config/powershell ~/.config/powershell
-ln -s $DOT_DIR/.config/tmux ~/.config/tmux
+
+ln -s $HOME/Git/dotfiles/.config/fish ~/.config/fish
+ln -s $HOME/Git/dotfiles/.config/nvim ~/.config/nvim
+ln -s $HOME/Git/dotfiles/.config/powershell ~/.config/powershell
+ln -s $HOME/Git/dotfiles/.config/tmux ~/.config/tmux
