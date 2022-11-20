76dc2e630d1a7b8e17234c31bbfca4c0f0fb1e42
ymktmk <ymktmk.tt@gmail.com>
2022-11-20 23:32:23 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 DOT_DIR="$HOME/Git/dotfiles"
 
-git clone https://github.com/{your account name}/dotfiles.git ${DOT_DIR}
+git clone https://github.com/ymktmk/dotfiles.git ${DOT_DIR}
 
 ln -sf $DOT_DIR/.zshenv ~/.zshenv
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
