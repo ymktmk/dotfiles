67b34a1cd59a5d12d8bd27a1930349a43b51279b
ymktmk <ymktmk.tt@gmail.com>
2022-11-21 00:19:23 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/ymktmk/dotfiles.git ${DOT_DIR}
 
 ln -sf $DOT_DIR/.zshenv ~/.zshenv
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
-ln -sf $DOT_DIR/.config ~/.config
-
-nvim ~/.config/nvim/init.lua
+ln -s $DOT_DIR/.config/fish ~/.config/fish
+ln -s $DOT_DIR/.config/nvim ~/.config/nvim
+ln -s $DOT_DIR/.config/powershell ~/.config/powershell
+ln -s $DOT_DIR/.config/tmux ~/.config/tmux
